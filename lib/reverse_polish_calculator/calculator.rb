ad96a8894b61814a5d2ec17d529e3bde7a797f49
Edward Anderson <nilbus@nilbus.com>
2013-02-23 21:49:34 -0500

    Calculator parses expressions from input and calculates the result

--- lib/reverse_polish_calculator/calculator.rb
+++ lib/reverse_polish_calculator/calculator.rb
@@ -1,6 +1,41 @@
 module ReversePolishCalculator
+  # Calculator parses expressions from input and calculates the result using MathEngine
   class Calculator
-    def calculate(expression, &block)
+    def initialize
+      @math_engine = MathEngine.new
+    end
+
+    def calculate(input, &block)
+      input = StringIO.new(input) unless input.respond_to? :readline
+      while expression = line_from(input)
+        begin
+          tokens = tokenize(expression)
+          @math_engine.process_tokens(tokens)
+          yield [@math_engine.stack, nil] if block_given?
+        rescue
+          yield [@math_engine.stack, $!.message] if block_given?
+        end
+      end
+    end
+
+    def tokenize(expression)
+      expression.split.map do |token|
+        numeric = Float(token) rescue false
+
+        if numeric
+          Float(token)
+        else
+          Operator.new(token)
+        end
+      end
+    end
+
+  private
+
+    def line_from(io)
+      io.readline
+    rescue EOFError
+      nil
     end
   end
 end
--- spec/reverse_polish_calculator/calculator_spec.rb
+++ spec/reverse_polish_calculator/calculator_spec.rb
@@ -5,23 +5,32 @@ describe ReversePolishCalculator::Calculator do
 
   describe '#calculate' do
     before :each do
-      ReversePolishCalculator::MathEngine.stub(:process_tokens)
-      ReversePolishCalculator::Operator.stub(:new)
+      ReversePolishCalculator::MathEngine.any_instance.stub(:process_tokens => nil, :stack => [])
+      ReversePolishCalculator::Operator.stub(:new => stub(:+))
     end
 
     it 'creates Operator objects for each operator' do
-      calculator.calculate("  1  2.0\n3 + 4\n")
       ReversePolishCalculator::Operator.should_receive(:new).once
+      calculator.calculate("  1  2.0\n3 + 4\n")
     end
 
     it 'sends the tokens in each line of input to MathEngine#process_tokens' do
+      ReversePolishCalculator::MathEngine.any_instance.should_receive(:process_tokens).twice
       calculator.calculate("1 2\n3 4 +")
-      ReversePolishCalculator::MathEngine.should_receive(:process_tokens).twice
     end
 
     it 'accepts both String and IO inputs' do
       calculator.calculate("1 2\n3 4 +")
       calculator.calculate(StringIO.new("1 2\n3 4 +"))
+    end
+
+    it 'yields the stack to the block for each line in the input' do
+      expect { |block| calculator.calculate("1 2\n3 4 +", &block) }.to yield_successive_args([anything, nil], [anything, nil])
+    end
+
+    it 'yields errors to the block' do
+      ReversePolishCalculator::Operator.stub(:new) { raise 'Exception!' }
+      expect { |block| calculator.calculate("3 4 +", &block) }.to yield_with_args([anything, 'Exception!'])
     end
   end
 end
